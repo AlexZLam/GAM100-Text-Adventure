f529d242811c8c8189dfc540628c0a6eb35caeb2
Diana <144166059+diana-f-e@users.noreply.github.com>
2026-02-25 07:47:15 -0800

    exit door done

--- TextAdventureVisualStudio/Source/ExitDoorFunctions.c
+++ TextAdventureVisualStudio/Source/ExitDoorFunctions.c
@@ -20,7 +20,7 @@ typedef struct WorldData WorldData;
 /* Helper: The action performed when the exit door is used. */
 void ExitDoor_Use(CommandContext context, GameState* gameState, WorldData* worldData)
 {
-	Item* goldPiece; /* the gold piece in the user's inventory */
+	Item* orb; /* the gold piece in the user's inventory */
 	Item* egg; /* the egg in the user's inventory */
 
 	/* avoid W4 warnings on unused parameters - this function conforms to a function typedef */
@@ -28,13 +28,13 @@ void ExitDoor_Use(CommandContext context, GameState* gameState, WorldData* world
 	UNREFERENCED_PARAMETER(worldData);
 
 	/* find the gold piece in the user's inventory */
-	goldPiece = ItemList_FindItem(gameState->inventory, "gold piece");
+	orb = ItemList_FindItem(gameState->inventory, "orb");
 
 	/* find the egg in the user's inventory */
 	egg = ItemList_FindItem(gameState->inventory, "egg");
 
 	/* check if both items are in the user's inventory */
-	if ((egg == NULL) || (goldPiece == NULL))
+	if ((egg == NULL) || (orb == NULL))
 	{
 		/* both items were not found - tell the user about the problem and take no further action */
 		printf("You must keep searching for more treasures before you can exit!\n");
